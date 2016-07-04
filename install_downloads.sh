dbceae146811b9d186260fea193660c32eb32dbf
RehabMan <racerrehabman@gmail.com>
2016-07-04 11:08:04 -0700

    install HackrNVMeFamily-*.kext if on EFI partition

--- install_downloads.sh
+++ install_downloads.sh
@@ -205,13 +205,17 @@ fi
 
 # install NVMeGeneric.kext if it is found in Clover/kexts
 # patch it so it is marked OSBundleRequired=Root
-
 EFI=`sudo ./mount_efi.sh /`
 if [[ -e "$EFI/EFI/CLOVER/kexts/Other/NVMeGeneric.kext" ]]; then
     cp -R "$EFI/EFI/CLOVER/kexts/Other/NVMeGeneric.kext" /tmp/NVMeGeneric.kext
     /usr/libexec/PlistBuddy -c "Add :OSBundleRequired string" /tmp/NVMeGeneric.kext/Contents/Info.plist
     /usr/libexec/PlistBuddy -c "Set :OSBundleRequired Root" /tmp/NVMeGeneric.kext/Contents/Info.plist
     install_kext /tmp/NVMeGeneric.kext
+fi
+# install HackrNVMEFamily-.* if it is found in Clover/kexts
+kext=`echo "$EFI"/EFI/CLOVER/kexts/Other/HackrNVMeFamily-*.kext`
+if [[ -e "$kext" ]]; then
+    install_kext "$kext"
 fi
 
 #if [[ $MINOR_VER -ge 11 ]]; then
