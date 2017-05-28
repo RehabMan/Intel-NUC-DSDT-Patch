646e24fd7cf365b737a294dc2159f21372eaab84
RehabMan <racerrehabman@gmail.com>
2017-05-27 21:20:45 -0700

    add Lilu.kext and IntelGraphicsFixup.kext

--- download.sh
+++ download.sh
@@ -27,6 +27,8 @@ download os-x-fake-pci-id RehabMan-FakePCIID
 download os-x-brcmpatchram RehabMan-BrcmPatchRAM
 download os-x-usb-inject-all RehabMan-USBInjectAll
 download os-x-null-ethernet RehabMan-NullEthernet
+download lilu RehabMan-Lilu
+download intelgraphicsfixup RehabMan-IntelGraphicsFixup
 #download os-x-acpi-debug RehabMan-Debug
 cd ..
 
